794ead7fdc02f18a403788973991b9ccd797a4e3
MesenWei <kuimaosheng@126.com>
2017-12-05 00:12:48 +0800

    zuul的基本使用

---
+++ ms4-gateway-zuul/src/main/java/com/mesen/GatewayZuulApplication.java
@@ -0,0 +1,19 @@
+package com.mesen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ *
+ * Created by maosheng on 2017/12/3
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayZuulApplication.class, args);
+	}
+}
